e3511b8a3cf96a8513693284b98859bb2ab944f2
orestinva <or3zt.rvh@gmail.com>
2017-05-17 12:05:06 +0300

    added schoolnet

--- src/main/java/ua/lviv/softserve/lv236/tasks/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Package with GUI Main class and task pack.
- *
- * @author Mykola Myndyk
- */
-package ua.lviv.softserve.lv236.tasks;
--- src/main/java/ua/lviv/softserve/lv236/tasks/taskPack/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Package that contains all tasks with general template class.
- */
-package ua.lviv.softserve.lv236.tasks.taskPack;
