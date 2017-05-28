8b1f914677e3ee48157347e06abaf377457d1d32
bleac <marianpulup@gmail.com>
2017-05-28 16:43:36 +0300

    change password lv-236 on root

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -9,7 +9,6 @@ import { SchoolNetHomeModule } from './home/home.module';
 import { SchoolNetAdminModule } from './admin/admin.module';
 import { SchoolNetAccountModule } from './account/account.module';
 import { SchoolNetEntityModule } from './entities/entity.module';
-import { SchoolNetUserHomeModule } from './user-home/user-home.module';
 import { SchoolNetHeadTeacherModule } from './headteacher/headteacher.module';
 import { SchoolNetPupilHomeModule } from './pupil-home/pupil-home.module';
 
@@ -38,7 +37,6 @@ import {
         SchoolNetAdminModule,
         SchoolNetAccountModule,
         SchoolNetEntityModule,
-        SchoolNetUserHomeModule,
         SchoolNetHeadTeacherModule,
         SchoolNetPupilHomeModule
     ],
