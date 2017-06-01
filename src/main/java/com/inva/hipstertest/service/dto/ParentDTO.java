1951a111ea066a56c04e553c4a530434dfe4454d
Marta Klachko <marta.klachko@gmail.com>
2017-06-01 23:59:02 +0300

    added security for forms, translation for i18n, changed UI design

--- src/main/java/com/inva/hipstertest/service/dto/ParentDTO.java
+++ src/main/java/com/inva/hipstertest/service/dto/ParentDTO.java
@@ -23,6 +23,15 @@ public class ParentDTO implements Serializable {
     private String firstName;
 
     private String lastName;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public String getFirstName() {
         return firstName;
