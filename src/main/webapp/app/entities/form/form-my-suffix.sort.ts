d978fe83c7bdc30c963d1f1d9e5d1a895a12d0c1
Marta Klachko <marta.klachko@gmail.com>
2017-06-01 23:03:51 +0300

    added sorting of pupils in alphabetic order

---
+++ src/main/webapp/app/entities/form/form-my-suffix.sort.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: "sortPipe"
+})
+ export  class SortPipe implements PipeTransform{
+    transform(value: any[]) : any {
+        return  value.sort().reverse();
+    }
+}
+
