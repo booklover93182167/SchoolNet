68ad631578dd9442b3b4863f33de8a5501e5b730
Marta Klachko <marta.klachko@gmail.com>
2017-06-01 15:48:52 +0300

    merging with dev-marta

--- src/main/java/com/inva/hipstertest/service/dto/PupilDTO.java
+++ src/main/java/com/inva/hipstertest/service/dto/PupilDTO.java
@@ -1,6 +1,8 @@
 package com.inva.hipstertest.service.dto;
 
 
+import com.inva.hipstertest.domain.Parent;
+
 import javax.validation.constraints.*;
 import java.io.Serializable;
 import java.util.HashSet;
@@ -20,6 +22,38 @@ public class PupilDTO implements Serializable {
     private Long userId;
 
     private Long formId;
+
+    private Set<ParentDTO> parents;
+
+    private String firstName;
+
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String pupilFirstName) {
+        this.firstName = pupilFirstName;
+    }
+
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String pupilLastName) {
+        this.lastName = pupilLastName;
+    }
+
+    public Set<ParentDTO> getParents() {
+        return parents;
+    }
+
+    public void setParents(Set<ParentDTO> parents) {
+        this.parents = parents;
+    }
 
     public Long getId() {
         return id;
