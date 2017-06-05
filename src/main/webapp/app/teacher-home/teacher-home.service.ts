c026567ecef2276edd8c99e357cf78d2b0972456
Yaroslav Soltys <ce.prosto@gmail.com>
2017-06-05 09:00:37 +0300

    LVJAVV-131: correct path in teacher-home.service

--- src/main/webapp/app/teacher-home/teacher-home.service.ts
+++ src/main/webapp/app/teacher-home/teacher-home.service.ts
@@ -5,7 +5,7 @@ import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import {Observable} from 'rxjs/Rx';
 import {DateUtils} from 'ng-jhipster';
-import {ScheduleMySuffix} from "../entities/schedule/schedule-my-suffix.model";
+import {ScheduleMySuffix} from '../entities/schedule/schedule-my-suffix.model';
 
 @Injectable()
 export class TeacherHomeService {
@@ -13,10 +13,7 @@ export class TeacherHomeService {
     private resourceUrlLesson = 'api/teacher-home/lessons/teacher';
     private resourceUrlForm = 'api/teacher-home/forms/teacher';
     private resourceUrlCurrentTeacher = 'api/teacher-home/teachers/current';
-    private resourceUrlSchedule = 'api/teacher-home/schedules/teacher';
-    private resourceUrlForTeacherUpdateSchedule = 'api/teacher-home/schedules/update';
-    private resourceUrlForTeacherFindSchedule = 'api/teacher-home/schedules/find';
-
+    private resourceUrlSchedule = 'api/teacher-home/schedules';
 
     constructor(private http: Http, private dateUtils: DateUtils) {
     }
@@ -34,21 +31,20 @@ export class TeacherHomeService {
     }
 
     querySchedule(teacherId: number): Observable<Response> {
-        return this.http.get(`${this.resourceUrlSchedule}/${teacherId}`)
+        return this.http.get(`${this.resourceUrlSchedule}/teacher/${teacherId}`)
             .map((res: any) => this.convertResponse(res));
     }
 
     updateHomework(schedule: ScheduleMySuffix): Observable<ScheduleMySuffix> {
         const copy: ScheduleMySuffix = Object.assign({}, schedule);
-
         copy.date = this.dateUtils.toDate(schedule.date);
-        return this.http.put(this.resourceUrlForTeacherUpdateSchedule, copy).map((res: Response) => {
+        return this.http.put(`${this.resourceUrlSchedule}/update`, copy).map((res: Response) => {
             return res.json();
         });
     }
 
     findSchedule(id: number): Observable<ScheduleMySuffix> {
-        return this.http.get(`${this.resourceUrlForTeacherFindSchedule}/${id}`).map((res: Response) => {
+        return this.http.get(`${this.resourceUrlSchedule}/find/${id}`).map((res: Response) => {
             const jsonResponse = res.json();
             jsonResponse.date = this.dateUtils
                 .convertDateTimeFromServer(jsonResponse.date);
