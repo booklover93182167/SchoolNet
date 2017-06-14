36c5a2c0ac028e4ed5a85d821c7d7d0b89d91b92
orestinva <or3zt.rvh@gmail.com>
2017-06-14 12:39:58 +0300

    added config for Spring Security, added authorization for JWT via Cookie

---
+++ src/main/java/com/inva/hipstertest/service/util/CookieUtil.java
@@ -0,0 +1,34 @@
+package com.inva.hipstertest.service.util;
+
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by inva on 13-Jun-17.
+ */
+public class CookieUtil {
+    public static void create(HttpServletResponse httpServletResponse, String name, String value, Boolean secure, Integer maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setSecure(secure);
+        cookie.setMaxAge(maxAge);
+        cookie.setPath("/");
+        httpServletResponse.addCookie(cookie);
+    }
+
+    public static void clear(HttpServletResponse httpServletResponse, String name) {
+        Cookie cookie = new Cookie(name, null);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(0);
+        cookie.setDomain("localhost");
+        httpServletResponse.addCookie(cookie);
+    }
+
+    public static String getValue(HttpServletRequest httpServletRequest, String name) {
+        Cookie cookie = WebUtils.getCookie(httpServletRequest, name);
+        return cookie != null ? cookie.getValue() : null;
+    }
+}
