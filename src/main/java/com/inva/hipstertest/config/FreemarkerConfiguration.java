60b24632864a78742a16acda97962b67c3931d11
Vlad Yavorsky <vlad.yavorsky.ua@gmail.com>
2017-06-15 13:02:37 +0300

    Added localization for FreeMarker

--- src/main/java/com/inva/hipstertest/config/FreemarkerConfiguration.java
+++ src/main/java/com/inva/hipstertest/config/FreemarkerConfiguration.java
@@ -1,16 +1,24 @@
 package com.inva.hipstertest.config;
 
 import freemarker.template.TemplateException;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.ui.freemarker.FreeMarkerConfigurationFactory;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.Properties;
 
 @Configuration
 public class FreemarkerConfiguration extends WebMvcConfigurerAdapter {
@@ -27,9 +35,14 @@ public class FreemarkerConfiguration extends WebMvcConfigurerAdapter {
     @Bean
     public FreeMarkerConfigurer freemarkerConfig() throws IOException, TemplateException {
         FreeMarkerConfigurationFactory factory = new FreeMarkerConfigurationFactory();
-        factory.setTemplateLoaderPath("classpath:templates");
+        factory.setTemplateLoaderPaths("classpath:org/springframework/web/servlet/view/freemarker", "classpath:templates");
         factory.setDefaultEncoding("UTF-8");
         FreeMarkerConfigurer result = new FreeMarkerConfigurer();
+
+//        Properties properties = new Properties();
+//        properties.setProperty("auto_import", "/spring.ftl as spring");
+//        result.setFreemarkerSettings(properties);
+
         result.setConfiguration(factory.createConfiguration());
         return result;
     }
@@ -38,5 +51,32 @@ public class FreemarkerConfiguration extends WebMvcConfigurerAdapter {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**")
             .addResourceLocations("/resources/");
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("locale/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
     }
 }
--- src/main/resources/templates/freemarkertest.ftl
+++ src/main/resources/templates/freemarkertest.ftl
@@ -1,43 +1,68 @@
+<#import "/spring.ftl" as spring/>
+
 <!DOCTYPE html>
-<html lang="en">
-<head><title>FreeMarker Spring MVC Hello World</title>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>FreeMarker Spring MVC Hello World</title>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $("#locales").change(function () {
+                var selectedOption = $('#locales').val();
+                if (selectedOption != ''){
+                    window.location.replace(window.location.pathname + '?lang=' + selectedOption);
+                }
+            });
+        });
+    </script>
+</head>
 <body>
-<div id="header">
-    <h2>
-        FreeMarker Spring MVC Hello World
-    </h2>
+
+<div style="float: right">
+    <select id="locales">
+        <option value=""><@spring.message "lang.select"/></option>
+        <option value="en"><@spring.message "lang.en"/></option>
+        <option value="ru"><@spring.message "lang.ru"/></option>
+        <option value="ua"><@spring.message "lang.ua"/></option>
+    </select>
 </div>
 
+<div id="header">
+    <h2><@spring.message "greeting"/></h2>
+</div>
 <div id="content">
     <script type="text/javascript" src="/temlates/test.js"></script>
 
     <fieldset>
-        <legend>Add School</legend>
+        <legend><@spring.message "school.add"/></legend>
         <form name="schoolDTO" action="/freemarker/freemarkertest/add" method="post">
-            Name: <input type="text" name="name"/> <br/>
-            Enabled: <select name="enabled">
-            <option value="true">True</option>
-            <option value="false">False</option>
-        </select>
-            <input type="submit" value="Save"/>
+            <@spring.message "school.name"/>: <input type="text" name="name"/>
+            <br/><@spring.message "school.enabled"/>:
+                <select name="enabled">
+                <option value="true"><@spring.message "true"/></option>
+                <option value="false"><@spring.message "false"/></option>
+            </select>
+            <br/><input type="submit" value="<@spring.message "save"/>"/>
         </form>
     </fieldset>
     <br/>
-    <a href="logout">Logout</a>
+    <a href="logout"><@spring.message "logout"/></a>
     <table>
         <tr>
-            <th colspan="2">Schools List</th>
+            <th colspan="2"><@spring.message "school.list"/></th>
         </tr>
         <tr>
-            <th>Name</th>
-            <th>ID</th>
+            <th><@spring.message "school.name"/></th>
+            <th><@spring.message "school.id"/></th>
         </tr>
-    <#list model["schoolList"] as school>
-        <tr>
-            <td><a href="/freemarker/teachers/${school.id}">${school.name}</a></td>
-            <td>${school.id}</td>
-        </tr>
-    </#list>
+        <#list model["schoolList"] as school>
+            <tr>
+                <td><a href="/freemarker/teachers/${school.id}">${school.name}</a></td>
+                <td>${school.id}</td>
+            </tr>
+        </#list>
     </table>
 </div>
 </body>
