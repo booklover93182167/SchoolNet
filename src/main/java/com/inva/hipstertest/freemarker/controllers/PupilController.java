9a6f0787039d8e0fc6650c00bd3de0122da453d8
Yaroslav Soltys <ce.prosto@gmail.com>
2017-06-20 23:37:08 +0300

    fix import

--- src/main/java/com/inva/hipstertest/freemarker/controllers/PupilController.java
+++ src/main/java/com/inva/hipstertest/freemarker/controllers/PupilController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
