4ad13acc4b0ec6b2ea6d410500948b0cde8f6685
VolodymyrH <volodymyr.holodynskyi@gmail.com>
2017-06-27 16:37:29 +0300

    settings. lesson controller

---
+++ src/main/java/com/inva/hipstertest/freemarker/controllers/LessonController.java
@@ -0,0 +1,31 @@
+package com.inva.hipstertest.freemarker.controllers;
+
+import com.inva.hipstertest.service.LessonService;
+import com.inva.hipstertest.service.dto.LessonDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+public class LessonController {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    private final LessonService lessonService;
+
+    public LessonController(LessonService lessonService) {
+        this.lessonService = lessonService;
+    }
+
+    @RequestMapping(value = "freemarker/teacher-mgmt/lessons", method = RequestMethod.GET)
+    public String lessonList(@ModelAttribute("model")ModelMap model) {
+        List<LessonDTO> lessonList = lessonService.findAll();
+        model.addAttribute("lessonList", lessonList);
+        return "teacher-mgmt/lessons";
+    }
+}
