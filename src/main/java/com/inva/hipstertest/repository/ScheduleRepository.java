a27d593698c4f0407c9811128766ad1faae1679a
Yaroslav Soltys <ce.prosto@gmail.com>
2017-07-08 19:55:18 +0300

    added queries to the "ScheduleRepository"

--- src/main/java/com/inva/hipstertest/repository/ScheduleRepository.java
+++ src/main/java/com/inva/hipstertest/repository/ScheduleRepository.java
@@ -26,13 +26,23 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     @Query("select schedule from Schedule schedule, Teacher teacher, School school where schedule.teacher.id = teacher.id and teacher.school.id = school.id and school.id = :schoolId")
     List<Schedule> findAllBySchoolId(@Param("schoolId") Long schoolId);
 
-//    @Query(value = "SELECT * FROM Schedule schedule WHERE schedule.form_id = :formId AND " +
-//        "YEAR(schedule.jhi_date) = YEAR (:date) AND MONTH(schedule.jhi_date) = MONTH (:date) AND DAY(schedule.jhi_date) = DAY (:date)", nativeQuery = true)
-//    List<Schedule> findAllByFormIdAndDate(@Param("date") String date, @Param("formId") Long formId);
-
     @Query("select schedule from Schedule schedule where schedule.form.id = :formId and " +
         "schedule.date between :startDate and :endDate")
-    List<Schedule> findAllMembersByFormIdAndDateBetween(@Param("formId") Long formId, @Param("startDate") ZonedDateTime startDate, @Param("endDate") ZonedDateTime endDate);
+    List<Schedule> findAllMembersByFormIdAndDateBetween(@Param("formId") Long formId,
+                                                        @Param("startDate") ZonedDateTime startDate,
+                                                        @Param("endDate") ZonedDateTime endDate);
+
+    @Query("select schedule from Schedule schedule where schedule.teacher.id = :teacherId and " +
+        "schedule.date between :startDate and :endDate")
+    List<Schedule> findAllByTeacherIdAndDateBetween(@Param("teacherId") Long formId,
+                                                    @Param("startDate") ZonedDateTime startDate,
+                                                    @Param("endDate") ZonedDateTime endDate);
+
+    @Query("select schedule from Schedule schedule where schedule.classroom.id = :classroomId and " +
+        "schedule.date between :startDate and :endDate")
+    List<Schedule> findAllMembersByClassroomIdAndDateBetween(@Param("classroomId") Long classroomId,
+                                                             @Param("startDate") ZonedDateTime startDate,
+                                                             @Param("endDate") ZonedDateTime endDate);
 
 //    Identify specific subjects for specific classes where the specific teacher gives lessons
 //    @Query("select s from Schedule s where s.enabled = true and s.teacher.id = :teacherId group by s.form.id, s.lesson.id")
@@ -46,5 +56,4 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
     @Query("select COUNT(s) from Schedule s where s.enabled = true and s.teacher.id = :teacherId and s.form.id = :formId and s.lesson.id = :lessonId and s.date <= :today order by s.date")
     Long countSchedulesForGradeBook(@Param("teacherId") Long teacherId, @Param("formId") Long formId, @Param("lessonId") Long lessonId, @Param("today") ZonedDateTime today);
-
 }
